292e5464dff6a5c724a53d147a1ea547c9874d9d
rnpacifique <rpacfique12@gmail.com>
2024-05-29 21:51:19 +0200

    return a promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+/* eslint-disable space-before-function-paren */
+export default function getFullResponseFromAPI (success) {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(new Error(
+          'The fake API is not working currently',
+        ));
+      }
+    });
+  }
