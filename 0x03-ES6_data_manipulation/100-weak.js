330e5f17871a40f995234af5745baab75954c473
rnpacifique <rpacfique12@gmail.com>
2024-06-05 00:34:19 +0200

    updates

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,26 +1,13 @@
-// Define a const instance of WeakMap and name it weakMap
-const weakMap = new WeakMap();
+export const weakMap = new WeakMap();
 
-// Export the weakMap constant
-export { weakMap };
+const MAX_ENDPOINT_CALLS = 5;
 
-// Define the queryAPI function
-function queryAPI(endpoint) {
-  // Check if the endpoint is already tracked in the weakMap
+export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    // If not, initialize the count to 1
-    weakMap.set(endpoint, 1);
-  } else {
-    // If it is, increment the count by 1
-    const count = weakMap.get(endpoint);
-    weakMap.set(endpoint, count + 1);
+    weakMap.set(endpoint, 0);
   }
-
-  // Check if the number of queries is >= 5
-  if (weakMap.get(endpoint) >= 5) {
-    // If it is, throw an error
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
     throw new Error('Endpoint load is high');
   }
 }
-
-export default queryAPI;
