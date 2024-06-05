7a12dc518d7b466a4f64bd9dd9ab143d7c36c0e8
rnpacifique <rpacfique12@gmail.com>
2024-06-05 22:12:16 +0200

    tasks

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,10 @@
+// Subject.ts
+namespace Subjects {
+    export class Subject {
+      private _teacher: Teacher;
+  
+      setTeacher(teacher: Teacher): void {
+        this._teacher = teacher;
+      }
+    }
+  }
