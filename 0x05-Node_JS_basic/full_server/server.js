c8282a2d5b69a5c881c5920c40a3249270f6eeb9
rnpacifique <rpacfique12@gmail.com>
2024-07-23 12:59:35 +0200

    task

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import routes from './routes';
+import mapRoutes from './routes';
 
 const app = express();
 const PORT = 1245;
 
-app.use(routes);
-
+mapRoutes(app);
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
 });
