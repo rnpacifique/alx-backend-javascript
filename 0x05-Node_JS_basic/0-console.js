638f8ae9799c2200db384b51af7d2be9b7b0afc6
rnpacifique <rpacfique12@gmail.com>
2024-07-23 12:01:24 +0200

    create a function named displayMessage that prints in STDOUT the string argument

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+
+const displayMessage = message => {
+	console.log(message);
+}
+
+module.exports = displayMessage;
